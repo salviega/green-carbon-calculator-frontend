950b8359b2a6067de18ae682ed77326386c0744a
gitguti <gitmelgtrrz1@gmail.com>
2023-08-23 18:39:58 -0400

    Create overview component

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -11,83 +11,104 @@ import {
 	Button,
 	Text,
 	VStack,
+  Spacer,
 	useBreakpointValue,
 	CardHeader,
-	Heading
+	Heading,
+  Progress,
+  Avatar,
+  HStack
 } from '@chakra-ui/react'
 import { useColorMode , useColorModeValue} from '@chakra-ui/react'
 const Dashboard = () => {
   const bg = useColorModeValue('red.500', 'red.200')
 	return (
-		<Card>
-			<CardBody>
 				<Grid
-					h='700px'
+					h='1000px'
 					templateRows='repeat(3, 1fr)'
-					templateColumns='repeat(5, 1fr)'
+					templateColumns='repeat(6, 1fr)'
 					gap={4}
 				>
-					<GridItem colSpan={3} borderRadius='lg' rowSpan={2}>
+					<GridItem colSpan={4} borderRadius='lg' rowSpan={1} borderColor="red.200" border="1px" p="4">
+            <Text> Overview</Text>
 						<Flex
-							h={'100%'}
+							h={'250px'}
 							backgroundImage={
 								'url(https://images.unsplash.com/photo-1600267175161-cfaa711b4a81?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)'
 							}
 							backgroundSize={'cover'}
 							borderRadius='lg'
+              pos="relative"
 						>
-							<VStack
+              <HStack  pos="absolute" bottom="2" left="2">
+                <Avatar name='T P' src='https://bit.ly/tioluwani-kolawole' size="sm" />
+                <Text>
+                Tropic project</Text>
+              </HStack>
+						</Flex>
+            <HStack
 								w={'full'}
-								justify={'center'}
-								px={useBreakpointValue({ base: 4, md: 8 })}
-								bgGradient={'linear(to-r, blackAlpha.600, transparent)'}
+                align="start"
+                pt="2"
 							>
-								<Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+								<Stack align='start' w="full">
 									<Text
-										color={'white'}
 										fontWeight={700}
 										lineHeight={1.2}
-										fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}
+										fontSize={useBreakpointValue({ base: 'xl', md: '2xl' })}
 									>
-										Lorem ipsum dolor sit amet consectetur adipiscing elit sed
-										do eiusmod tempor
+										$120,8 K
 									</Text>
-									<Stack direction={'row'}>
+                  <Text>Raised from 213 contributors</Text>
+                  <Progress colorScheme='green' size='lg' value={33} w="100%" borderRadius="lg"/>
+								</Stack>
+                <Stack direction={'column'} w="1/2" alignItems="flex-end">
+                  <Spacer/>
+                  <Spacer/>
+                  <Spacer/>
+                  <Spacer/>
+                  <Spacer/>
 										<Button
-											bg={'blue.400'}
-											rounded={'full'}
+											bg={'black'}
+                      size="lg"
+											rounded={'md'}
 											color={'white'}
 											_hover={{ bg: 'blue.500' }}
 										>
-											Show me more
-										</Button>
-										<Button
-											bg={'whiteAlpha.300'}
-											rounded={'full'}
-											color={'white'}
-											_hover={{ bg: 'whiteAlpha.500' }}
-										>
-											Show me more
+											Donate
 										</Button>
 									</Stack>
-								</Stack>
-							</VStack>
-						</Flex>
+							</HStack>
+
 					</GridItem>
-					<GridItem rowSpan={3} colSpan={2} borderRadius='lg' bg='gray.100'>
-							<CardHeader>
-								<Heading size='md'>Client Report</Heading>
-							</CardHeader>
+					<GridItem rowSpan={1} colSpan={2} borderRadius='lg' bg='gray.100'>
+              <Text> CO2 Amount</Text>
 					</GridItem>
 					<GridItem
 						colSpan={3}
 						borderRadius='lg'
 						bg='gray.100'
 						rowSpan={1}
-					></GridItem>
+					>
+            Event List
+          </GridItem>
+          					<GridItem
+						colSpan={3}
+						borderRadius='lg'
+						bg='gray.600'
+						rowSpan={1}
+					>
+            Event Details
+          </GridItem>
+          					<GridItem
+						colSpan={6}
+						borderRadius='lg'
+						bg='gray.600'
+						rowSpan={1}
+					>
+            Historical
+          </GridItem>
 				</Grid>
-			</CardBody>
-		</Card>
 	)
 }
 
