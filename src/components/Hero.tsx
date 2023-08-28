bd0ad65f085506565d56ae0791eb77d16600c719
gitguti <gitmelgtrrz1@gmail.com>
2023-08-27 23:12:40 -0500

    add border to nft hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
         </Stack>
       </VStack>
       <Flex align="center" justify="center">
-      <Box bg='brand.newBlack' opacity="90%" px="4" maxH="md" py="6" width="300px" justifyContent="center" borderRadius="lg">
+      <Box bg='brand.newBlack' opacity="90%" px="4" maxH="md" py="6" width="300px" justifyContent="center" borderRadius="lg" border="1px" borderColor="whiteAlpha.200">
       <Image
         objectFit='cover'
         src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
