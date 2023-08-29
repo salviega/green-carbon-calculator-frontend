d489a6db07b9a8d201d34f22beb88415f772c9e3
gitguti <gitmelgtrrz1@gmail.com>
2023-08-29 17:30:19 -0500

    update event color indicator

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Button, Text, Circle, VStack, HStack } from '@chakra-ui/react'
 import { Event } from '@/models/project.model'
+import {PiTree} from 'react-icons/pi'
 export interface EventListProps {
 	events: Event[]
 	owner: boolean
@@ -12,19 +13,21 @@ export default function EventList({ events, owner }: EventListProps) {
 				return (
 					<HStack overflow='hidden' justify='space-between' pb='4' key={event.event_id}>
 						<HStack  w='80%'>
-							<Circle size='36px' bg={event.isCertified? 'green.200' : 'gray.200'} />
+							<Circle size='12' bg={event.isCertified? 'brand.light' : 'gray.100'}>
+              <PiTree  size='24'/>
+              </Circle>
 							<VStack gap='0' align='start'>
-								
+
 								<Text fontWeight='semibold' fontSize='md' textColor='gray.700' maxW="100%">
 									{' '}
 									{event.name}
 								</Text>
 							</VStack>
 						</HStack>
-						<Button variant='solid' colorScheme='teal' size='sm'>
+						<Button variant='outline' size='sm'>
 							View
 						</Button>
-            {owner&& <Button variant='solid' size='sm' colorScheme='orange'>
+            {owner&& <Button variant='solid' size='sm' colorScheme='red'>
 							Delete
 						</Button>}
 					</HStack>
