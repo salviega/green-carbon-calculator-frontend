6e11660ea373698ba986196212503153aae2389f
Sebas <sheva3232@gmail.com>
2023-08-30 08:34:59 -0500

    fixing special symbols

--- src/pages/myprojects/index.tsx
+++ src/pages/myprojects/index.tsx
@@ -1,27 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { getAccount } from '@wagmi/core'
-import {
-	Stack,
-	Flex,
-	Spacer,
-	Button,
-  Container,
-	Text,
-  Center,
-	VStack,
-	useBreakpointValue,
-	Box,
-	HStack,
-	Image,
-	SimpleGrid,
-	GridItem
-} from '@chakra-ui/react'
+import { Container, Text, Center, VStack } from '@chakra-ui/react'
 import Projectsection from '../../components/ProjectsSection'
 import Head from 'next/head'
-import { firebaseApi } from '../../../services/firebaseApi'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useAccount } from 'wagmi'
-
 
 const metadata = {
 	title: 'Footprint',
@@ -30,21 +13,16 @@ const metadata = {
 
 const MyProjects = () => {
 	const account = getAccount()
-	const { getProjectsByOwnerAddress } = firebaseApi()
 	const [walletConnected, setWalletConnected] = useState(false)
-	const { address, isConnecting, isDisconnected, isConnected} = useAccount({
-    onConnect({ address, connector, isReconnected }) {
+	const { address } = useAccount({
+		onConnect({ address, connector, isReconnected }) {
 			setWalletConnected(true)
-      readProjects();
-    },
-    onDisconnect() {
+		},
+		onDisconnect() {
 			setWalletConnected(false)
-      console.log('Disconnected')
-    },
-  })
-  const readProjects = async () => {
-		console.log('read projects');
-  }
+			console.log('Disconnected')
+		}
+	})
 	return (
 		<>
 			<Head>
@@ -53,23 +31,42 @@ const MyProjects = () => {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/Images/favicon.ico' sizes='any' />
 			</Head>
-			{walletConnected ?
-			<>
-        <Text as="h2" fontSize="5xl" marginY="12" fontWeight="bold" textColor="brand.newBlack">My projects</Text>
-				<Projectsection />
-			</>
-			:
-			<>
-        <Container>
-        <Center h="800px" w='100%'>
-        <VStack>
-        <Text fontSize="5xl" fontWeight="bold" textColor="brand.newBlack">We don't bite🥹</Text>
-        <Text fontSize="lg"  textColor="gray.600" mb='4'>Please connect your wallet to see all your awesome projects🌴</Text>
-        <ConnectButton />
-        </VStack>
-        </Center>
-        </Container>
-			</>}
+			{walletConnected ? (
+				<>
+					<Text
+						as='h2'
+						fontSize='5xl'
+						marginY='12'
+						fontWeight='bold'
+						textColor='brand.newBlack'
+					>
+						My projects
+					</Text>
+					<Projectsection />
+				</>
+			) : (
+				<>
+					<Container>
+						<Center h='800px' w='100%'>
+							<VStack>
+								<Text
+									fontSize='5xl'
+									fontWeight='bold'
+									textColor='brand.newBlack'
+								>
+									{"We don't bite🥹"}
+								</Text>
+								<Text fontSize='lg' textColor='gray.600' mb='4'>
+									{
+										'Please connect your wallet to see all your awesome projects🌴'
+									}
+								</Text>
+								<ConnectButton />
+							</VStack>
+						</Center>
+					</Container>
+				</>
+			)}
 		</>
 	)
 }
